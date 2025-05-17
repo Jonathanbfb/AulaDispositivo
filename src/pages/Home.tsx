5b094202981c480402fdb0d47a34ed514a90c2c9
Jonathanbfb <ti_jonahan@hotmail.com>
2025-05-16 21:27:34 -0400

    Salvando o código

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonInput, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { useState } from 'react';
 
 const Home: React.FC = () => {
+
+  const [digite, setDigite] = useState(0);
+  const [resultado, setResultado] = useState <number | null> (null) ;
+
+  const CalcularArea = () => {
+    setResultado(3.14 * digite * digite);
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Curso de Dispositivos Móveis</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +25,17 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+       
+       <h1>Criando aplicativo IONIC</h1>
+       <IonInput onIonChange={(e) => setDigite(parseFloat(e.detail.value!)) } 
+       label="Digite o valor do Raio" 
+       placeholder="Digite o nº"></IonInput>
+       <IonButton onClick={CalcularArea} >Calcular</IonButton>
+       <IonText color="primary">
+        <h1>o valor da área é: {resultado}</h1>
+      </IonText>
+
+
       </IonContent>
     </IonPage>
   );
